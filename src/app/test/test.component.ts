335918dc1b9b26da0f6f6e882105f56002321ba4
Kanak Pravin Chauhan -X (kachauha - INFOSYS LIMITED at Cisco) <kachauha@partnet.cisco.com>
2018-09-14 19:41:35 +0530

    update

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <div><input  [id]="myID" type="text" value="Abhijeet"/></div>
+    <div> <input  [id]="myID" type="text" value="Abhijeet"/> </div>
+    <div> <input  id="{{myID}}" type="text" value="Abhijeet G"/> </div>
+    <div> <input  disabled type="text" value="Abhijeet Kumar"/> </div>
+    <div> <input  disabled="false" type="text" value="Abhijeet Kumar 1"/> </div>
+    <div> <input  [disabled]="false"  type="text" value="Abhijeet Kumar 2"/> </div>
+    <div> <input  disabled="{{false}}"  type="text" value="Abhijeet Kumar 3"/> </div>
+
   `,
   styles: []
 })
